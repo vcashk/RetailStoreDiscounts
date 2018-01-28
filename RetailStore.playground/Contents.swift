5398d2ac21087faf3beb838137b41abf61879f1e
vcashk <vcashk@gmail.com>
2018-01-28 15:08:59 +0400

    added test cases

--- RetailStore.playground/Contents.swift
+++ RetailStore.playground/Contents.swift
@@ -4,19 +4,46 @@ import UIKit
 import XCTest
 
 
-protocol Discount {
+
+class Product {
+    var prodName: String
+    var prodPrice: Int
     
-     func calculateDiscount() -> Float
+    init(prodName: String, prodPrice: Int){
+        self.prodName = prodName
+        self.prodPrice = prodPrice
+    }
 }
+//protocol Discount {
+//
+//    func calculateDiscount() -> Float
+//}
+
+class Customer {
+//    var custname: String
+//    var loyaltyYears: Int
+//    
+//    init(custname: String, loyaltyYears: Int){
+//        self.custname = custname
+//        self.loyaltyYears = loyaltyYears
+//    }
+    
+//    func calculateDiscount() -> Float {
+//        if self.loyaltyYears >= 2 {
+//            return 5
+//        }
+//    }
+    
+}
+
+
 
 class UserCategory<T> {
     var category: T
     init(category:T) {
         self.category = category
     }
-   
 
-   
 }
 
 class DiscountRules {
@@ -31,44 +58,35 @@ class DiscountRules {
     }
     
     
-    
-//    mutating func checkProd(){
-//
-//        if (self.product) = "Grocery"{
-//            print ("no discount")
-//        factor = round(cost / 100.0)
-//    }
-//    }
-    
-    
 }
 
 
-    
 
-class Employee: Discount {
+class Employee:Customer {
     var name : String
     var discount : Float
+    var product: Product
 
-    init(name: String, discount: Float) {
+    init(name: String, discount: Float, product: Product) {
         
         self.name = name
         self.discount = discount
+        self.product = product
     }
     
     func calculateDiscount() -> Float {
         return self.discount
     }
     
-    
 }
 
-class Affliate: Discount {
+class Affliate: Customer {
     var name : String
     var discount : Float
     var affliateDiscription : String
     
-    init(name: String, discount: Float, affliateDiscription: String ) {
+    init(name: String, discount: Float, affliateDiscription: String) {
+        
         self.name = name
         self.discount = discount
         self.affliateDiscription = affliateDiscription
@@ -79,25 +97,6 @@ class Affliate: Discount {
     }
 }
 
-
-class Customer:Discount {
-    var name : String
-    var discount : Float
-    var loyaltyyears : Int
-    
-    init(name: String, loyaltyyears: Int, discount: Float) {
-        self.name = name
-        self.discount = discount
-        self.loyaltyyears = loyaltyyears
-    }
-    
-    func calculateDiscount() -> Float {
-        return self.discount
-    }
-    
-    
-    
-}
 
 
 class User {
@@ -113,14 +112,8 @@ class User {
 class Users {
         var users : Array<Int> = []
     }
-    
-let employee1 = Employee(name: "Victor", discount: 30 )
-employee1.calculateDiscount()
-let affliate1 = Affliate(name: "Affliate1", discount: 10.0 , affliateDiscription: "Affliate of group C")
-affliate1.calculateDiscount()
 
-let user1 = User(userCategory: UserCategory(category: employee1))
-let user2 = User(userCategory: UserCategory(category: affliate1))
+
 
 
 //MARK: Helper Functions
@@ -147,16 +140,31 @@ class RDTest: XCTestCase {
         super.setUp()
     }
     
-    
-    
+    func testRetailDiscount(){
+        
+        //creating prod1
+        let prod1 = Product(prodName: "grocerry", prodPrice: 100)
+        let employee1 = Employee(name: "Victor", discount: 30, product: prod1)
+        employee1.calculateDiscount()
+        let affliate1 = Affliate(name: "Affliate1", discount: 10.0 , affliateDiscription: "Affliate of group C")
+        affliate1.calculateDiscount()
+        //let customer1 = Customer(name: "customer1", loyaltyyears: 2, discount: 5.0)
+        
+        
+        
+        _ = User(userCategory: UserCategory(category: employee1))
+        _ = User(userCategory: UserCategory(category: affliate1))
+    }
+ 
+}
 //    XCTAssertTrue(, "check1 success.")
 //    XCTAssertNotNil(, " instance not created..")
     
-}
+
 
 
 
 
 //execute bst tests
-//RDTest.defaultTestSuite.run()
+RDTest.defaultTestSuite.run()
 
