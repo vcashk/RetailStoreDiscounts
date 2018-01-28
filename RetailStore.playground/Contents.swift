8168e1d7085b30277ef4d3613875e78c63600e54
vcashk <vcashk@gmail.com>
2018-01-28 12:14:49 +0400

    added generics

--- RetailStore.playground/Contents.swift
+++ RetailStore.playground/Contents.swift
@@ -1,11 +1,21 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import XCTest
 
-
-struct UserCategory<T> {
+final class UserCategory<T> {
+    var category: T
+    init(category:T) {
+        self.category = category
+    }
+    
     
 }
+
+let user1 = UserCategory(category: "Employee")
+print(user1.category)
+
+
 
 final class User<T> {
     var value: T
@@ -13,11 +23,31 @@ final class User<T> {
         self.value = value
     }
     
-}
 
-class Users {
     
 }
 
 
+
+
+class RDTest: XCTestCase {
+    
+    
+    //called before each test invocation
+    override func setUp() {
+        super.setUp()
+    }
+    
+    
+    
+//    XCTAssertTrue(, "check1 success.")
+//    XCTAssertNotNil(, " instance not created..")
+    
+}
+
+
+
+
+//execute bst tests
+//RDTest.defaultTestSuite.run()
 
