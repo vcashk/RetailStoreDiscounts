0b0cd0b017086d16c9cfd94649626a87358d7d80
vcashk <vcashk@gmail.com>
2018-01-28 11:58:27 +0400

    initial retailstore playground

---
+++ RetailStore.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+struct UserCategory<T> {
+    
+}
+
+final class User<T> {
+    var value: T
+    init(value:T) {
+        self.value = value
+    }
+    
+}
+
+class Users {
+    
+}
+
+
+
